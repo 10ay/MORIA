b3a12c34a6a702cd7c01869012e4731e47feffe9
10ay <tanaymbhadra@berkeley.edu>
2026-03-11 11:36:52 -0400

    pip

--- src/.ipynb_checkpoints/__init__-checkpoint.py
+++ src/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,2 +1,4 @@
-from .reduce import *
-
+"""
+    import moria
+    from moria import matchup_files, extract_psf_1
+"""
--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,4 @@
 """
-Top-level namespace is now provided by the `moria` package.
-
-This module is intentionally left minimal; import from `moria` instead:
-
     import moria
     from moria import matchup_files, extract_psf_1
 """
